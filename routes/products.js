0d38822920af079bb1e170c22c71c4800071dbf8
Mariano Vazquez <mvazquezmultimedia@gmail.com>
2024-06-18 19:40:59 -0300

    agrego el endpoint de products para el CRUD

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET products listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with products');
 });
 
 module.exports = router;
-
-
